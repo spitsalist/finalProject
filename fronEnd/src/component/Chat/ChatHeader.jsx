4638f5fe4a65442ae3b728ba7d5396ec84ba48a8
Vadim <vadim.redpoint@gmail.com>
2025-01-19 19:47:15 +0100

    fixed chatHeader/nav/ptofile_id

--- fronEnd/src/component/Chat/ChatHeader.jsx
+++ fronEnd/src/component/Chat/ChatHeader.jsx
@@ -1,7 +1,12 @@
 import { Box, Avatar, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const ChatHeader = ({ selectedUserInfo }) => {
+  const navigate = useNavigate()
   if (!selectedUserInfo) return null;
+  const handleNavigateToProfile =()=>{
+    navigate(`/profile/${selectedUserInfo._id}`)
+  }
   return (
     <Box
       sx={{
@@ -14,9 +19,10 @@ export const ChatHeader = ({ selectedUserInfo }) => {
       <Avatar
         src={selectedUserInfo.profileImage}
         alt={selectedUserInfo.username}
-        sx={{ width: 48, height: 48, marginRight: "16px" }}
+        sx={{ width: 48, height: 48, marginRight: "16px", cursor:'pointer', transition:'opacity 0.3s ease', '&:hover': { opacity: 0.7} }}
+        onClick={handleNavigateToProfile}
       />
-      <Typography variant="h6">{selectedUserInfo.username}</Typography>
+      <Typography sx={{cursor:'pointer', transition: 'color 0.3 ease', '&:hover': {color:'primary.main'}}} onClick={handleNavigateToProfile} variant="h6">{selectedUserInfo.username}</Typography>
     </Box>
   );
 };
